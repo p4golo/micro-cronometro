3a883f17f92e08f1a723eb861a9d6db0bee15a56
Francisco <pagolo2000@gmail.com>
2020-12-05 18:01:37 +0100

    tiene lo basico

---
+++ cronometro.py
@@ -0,0 +1,24 @@
+from microbit import *
+
+zero = "99999:99999"
+one = "00000:99999"
+two = "90999:99909"
+three = "90909:99999"
+four = "99900:09999"
+five = "99909:90999"
+six = "99999:90999"
+seven = "90900:99999"
+eight = "99099:99099"
+nine = "99900:99999"
+
+fonts = (zero, one, two, three, four, five, six, seven, eight, nine)
+
+def main():
+
+    while True:
+
+        display.set_pixel()
+
+
+if __name__ = "__main()__":
+    main()
