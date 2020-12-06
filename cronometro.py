e77d1e3bd8eda23b804113c032394349e36cc3b3
Francisco <pagolo2000@gmail.com>
2020-12-06 12:03:51 +0100

    muestra segundos y organizacion de funciones

--- cronometro.py
+++ cronometro.py
@@ -13,12 +13,46 @@ nine = "99900:99999"
 
 fonts = (zero, one, two, three, four, five, six, seven, eight, nine)
 
+columnas = 5
+filas = 5
+
+def mostrarNumeros():
+    pass
+
+def mostrarSegundos(segundos):
+    if segundos > 9:
+        decenas = segundos // 10
+        unidades = segundos % 10
+    else:
+        unidades = fonts[segundos]
+        unidades = unidades.split(":")
+        for i in range(filas):
+            display.set_pixel(3,i,int(unidades[0][i])) #Esto es lo que hay que usar para printear
+        for i in range(filas):
+            display.set_pixel(4,i,int(unidades[1][i]))
+
+def mostrarHoras(segundos):
+    pass
+
+def mostrarMinutos(segundos):
+    pass
+
 def main():
 
+    segundos = 0
+    minutos = 0
+    horas = 0
+
     while True:
+        #display.set_pixel() Esto es lo que hay que usar para printear
+        if button_a.is_pressed():
+            mostrarHoras(segundos)
+        if button_b.is_pressed():
+            mostrarMinutos(segundos)
+        mostrarNumeros(segundos)
+        sleep(1000) # Para que el bucle tarde un segundo en hacerse
+        segundos += 1
+        display.clear()
 
-        display.set_pixel()
-
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
