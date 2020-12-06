29368bea9345d228e555d9240403724cb890f433
Francisco <pagolo2000@gmail.com>
2020-12-06 17:19:30 +0100

    terminado y comentado

--- cronometro.py
+++ cronometro.py
@@ -1,6 +1,5 @@
 from microbit import *
 
-COLUMNAS = 5
 FILAS = 5
 PRIMERACOLUMNA = 0
 TERCERACOLUMNA = 3
@@ -18,25 +17,40 @@ nine = "99900:99999"
 
 fonts = (zero, one, two, three, four, five, six, seven, eight, nine)
 
-
-
 def mostrarNumeros(numero,posicion):
-        for i in range(FILAS):
+    """
+        Esta funcion muestra en el panel del microbit un numero dado y en la columna seleccionada.
+        @param numero: numero entero de 2 digitos
+        @param posicion: numero de la columna en la que se quiere mostrar el numero.
+    """
+        for i in range(FILAS): # Recorre todas las filas y enciende los led de la columna seleccionada con los valores dados
             display.set_pixel(posicion,i,int(numero[0][i]))
-        for i in range(FILAS):
+        for i in range(FILAS): # Recorre todas las filas y enciende los led de la siguiente columna de la seleccionada con los valores dados
             display.set_pixel(posicion + 1,i,int(numero[1][i]))
 
 def obtenerDecenas(numero):
+    """
+        Esta funcion devuelve las decenas del numero dado, con el valor necesario para representarlo en el microbit.
+        @param numero: numero entero
+    """
     decenas = numero // 10
     decenas = fonts[decenas].split(":")
     return decenas
 
 def obtenerUnidades(numero):
+    """
+        Esta funcion devuelve las unidades del numero dado, con el valor necesario para representarlo en el microbit.
+        @param numero: numero entero
+    """
     unidades = numero % 10
     unidades = fonts[unidades].split(":")
     return unidades
 
 def mostrarSegundos(segundos):
+    """
+        Esta funcion muestra en el panel del microbit el numero de segundos que han pasado.
+        @param segundos: numero entero de segundos que han pasado.
+    """
     if segundos > 9:
         decenas = obtenerDecenas(segundos)
         unidades = obtenerUnidades(segundos)
@@ -47,6 +61,10 @@ def mostrarSegundos(segundos):
         mostrarNumeros(unidades,TERCERACOLUMNA)
 
 def mostrarHoras(horas):
+    """
+        Esta funcion muestra en el panel del microbit el numero de horas que han pasado.
+        @param horas: numero entero de horas que han pasado.
+    """
     if horas > 9:
         decenas = obtenerDecenas(horas)
         unidades = obtenerUnidades(horas)
@@ -58,6 +76,10 @@ def mostrarHoras(horas):
     sleep(1000)
 
 def mostrarMinutos(minutos):
+    """
+        Esta funcion muestra en el panel del microbit el numero de minutos que han pasado.
+        @param minutos: numero entero de minutos que han pasado.
+    """
     if minutos > 9:
         decenas = obtenerDecenas(minutos)
         unidades = obtenerUnidades(minutos)
@@ -85,7 +107,7 @@ def main():
             display.clear()
         mostrarSegundos(segundos)
         sleep(1000) # Para que el bucle tarde un segundo en hacerse
-        segundos += 1
+        segundos += 1 # Aumentamos el valor de los segundos
         if segundos == 60:
             segundos = 0
             minutos += 1
